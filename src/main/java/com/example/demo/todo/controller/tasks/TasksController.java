e63d03a42055c4767efbbcb79d42fea847d84cc0
hondaminori <hondaminori.ta3@gmail.com>
2026-03-02 17:12:02 +0900

    複数件のデータを渡す

--- src/main/java/com/example/demo/todo/controller/tasks/TasksController.java
+++ src/main/java/com/example/demo/todo/controller/tasks/TasksController.java
@@ -1,5 +1,7 @@
 package com.example.demo.todo.controller.tasks;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TasksController {
     @GetMapping("/tasks")
     public String list(Model model) {
-        var task = new TaskDTO(
+        var task1 = new TaskDTO(
             1L,
             "Spring Bootを学ぶ" ,
             "TODOアプリケーションを作ってみる" ,
             "TODO"
         );
-        model.addAttribute("task", task);
+        var task2 = new TaskDTO(
+            2L,
+            "Spring Securtyを学ぶ" ,
+            "ログインを作ってみる" ,
+            "TODO"
+        );
+        var taskList = List.of(task1, task2);
+        model.addAttribute("taskList", taskList);
         return "tasks/list";
     }
 }
