b0b5875133f5ce6bcaa81a624aaa2af24c13ee3c
hondaminori <hondaminori.ta3@gmail.com>
2026-03-04 18:11:14 +0900

    更新時のバリデーションまで完了 95. 編集内容をバリデーションしよう　まで完了

--- src/main/java/com/example/demo/todo/controller/tasks/TasksController.java
+++ src/main/java/com/example/demo/todo/controller/tasks/TasksController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.todo.service.tasks.TaskService;
@@ -33,25 +34,51 @@ public class TasksController {
 
     @GetMapping("/{id}")
     public String showDetail(@PathVariable("id") long taskId, Model model) {
-        var taskEntity = taskService.findById(taskId)
-        .orElseThrow(() -> new IllegalArgumentException("Task not found id:" + taskId));
-        model.addAttribute("task", TaskDTO.toDTO(taskEntity));
+        var taskDTO = taskService.findById(taskId)
+            .map(TaskDTO::toDTO)
+            .orElseThrow(TaskNotFoundException::new);
+        model.addAttribute("task", taskDTO);
         return "tasks/detail";
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm(@ModelAttribute TaskForm form) {
+    public String showCreationForm(@ModelAttribute TaskForm form, Model model) {
+        model.addAttribute("mode", "CREATE");
         return "tasks/form";
     }
 
     @PostMapping
-    public String create(@Validated TaskForm form, BindingResult bindingResult) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return showCreationForm(form);
+            return showCreationForm(form, model);
         }
         form.toEntity();
         taskService.create(form.toEntity());
         return "redirect:tasks";
+    }
+
+    @GetMapping("/{id}/editForm")
+    public String showEditForm(@PathVariable("id") long id, Model model) {
+        var form = taskService.findById(id)
+            .map(TaskForm::fromEntity)
+            .orElseThrow(TaskNotFoundException::new);
+        model.addAttribute("taskForm", form);
+        model.addAttribute("mode", "EDIT");
+        return "tasks/form";
+    }
+
+    @PutMapping("{id}")
+    public String update(
+        @PathVariable("id") long id, 
+        @Validated @ModelAttribute TaskForm form, 
+        BindingResult bindingResult,
+        Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("mode", "EDIT");
+            return "tasks/form";
+        }
+        return "redirect:/tasks/{id}";
     }
 }
 
